3a0b18b8b4ca8775125fe64c4317f8414125b7a3
oscar <oscar.aguirre.tarazona@gmail.com>
2023-02-20 18:12:11 -0500

    metodos de array

--- 08-arrays-metodos.js
+++ 08-arrays-metodos.js
@@ -22,3 +22,38 @@ Array.prototype.pop () // elimina el ultimo
 Array.prototype.unshift () // agrega el elemento al incio
 Array.prototype.shift () // elimina el primer elemento
 Array.prototype.indexOf () // retorna el indice del array
+
+let articulos = [
+  {nombre: "Bicicleta", costo: 850},
+  {nombre: "TV", costo: 2550},
+  {nombre: "Laptop", costo: 8550},
+  {nombre: "Libro", costo: 150},
+  {nombre: "Celular", costo: 1850},
+  {nombre: "Teclado", costo: 430},
+]
+
+// FILTER general una nuevo array
+let articuloFiltados = articulos.filter(function (articulo){
+  return articulo.costo <= 500;
+});// articuloFiltados = [ {nombre: "Libro", costo: 150}, {nombre: "Teclado", costo: 430}]
+
+
+// MAP genera un nuevo array
+let nombreArticulo = articulos.map(function(articulo) {
+  return articulo.nombre;
+}); // nombreArticulo = [Bicicleta, TV, Laptop, Libro, Celular, Teclado]
+
+//FIND valida y genera un array
+let encuentraArticulo = articulos.find(function(articulo) {
+  return articulo.nombre === "Laptop";
+});// encuentraArticulo= {nombre: "Laptop", costo: 8550},
+
+//FOREACH 
+articulos.forEach(function(articulo){
+  console.log(articulo.nombre);
+}); // solo regresa los valores como "Bicicleta" "TV" "Laptop" "Libro" "Celular" "Teclado"
+
+// SOME es una validadcion
+let articulosBaratos = articulos.some(function(articulo) {
+  return articulo.costo <= 1500;
+});// true
